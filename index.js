152872d894107514376be36a774f39593b96f649
luis <luis.fer160197@gmail.com>
2023-11-30 15:48:18 -0600

    Finish with css help for the grid of chatGPT

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
 const container = document.querySelector('.container');
+const newBtn = document.querySelector('#new_grid');
 
 let num = 16;
+let num2 = num * num;
 
-for(let i = 0; i < num ; i++) {
-  const row = document.createElement('div');
-  row.classList.toggle('row');
-  for(let i = 0; i < num; i++) {
-    const square = document.createElement('div');
-    square.classList.toggle('square');
-    row.appendChild(square);
-  }
-  container.appendChild(row);
-};
+for(let i = 0; i < num2 ; i++) {
+  const square = document.createElement('div');
+  square.classList.toggle('square');
+  square.innerHTML = '&nbsp;';
+  container.appendChild(square);
+}
+
+let grid = document.querySelectorAll('.square');
+
+grid.forEach(element => {
+  element.addEventListener('mouseenter', () => {
+    element.classList.add('painted');
+  })
+});
+
+newBtn.addEventListener('click', function() {
+  let number = +prompt('Número de lados de nueva cuadrícula:');
+  if (Number.isInteger(number) && number <= 100 && number > 0) {
+    let number2 = number * number;
+
+    // Eliminar cada cuadro actual
+    grid.forEach(element => {
+      element.remove();
+    });
+
+    // Re hacer la rejilla
+    for(let i = 0; i < number2 ; i++) {
+      const square = document.createElement('div');
+      square.classList.toggle('square');
+      square.innerHTML = '&nbsp;';
+      container.appendChild(square);
+    }
+    grid = document.querySelectorAll('.square');
+    grid.forEach(element => {
+      element.addEventListener('mouseenter', () => {
+        element.classList.add('painted');
+      })
+    });
+
+    grid.forEach(square => {
+      square.background = 'black';
+      square.style.flexBasis = `calc(720px / ${number})`;
+      square.style.height = `calc(720px / ${number})`;
+      square.classList.remove('painted');
+    });
+  } else alert('Wrong input');
+});
